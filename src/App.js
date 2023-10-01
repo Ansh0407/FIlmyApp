572c3041b82102c8a5fdc29551e106d0d819c885
Ansh Bhandari <97969287+Ansh0407@users.noreply.github.com>
2023-10-01 16:45:47 +0530

    FInal FilmyApp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Routes from './Routes';
 
-function App() {
+import { useEffect, useState } from 'react';
+import './App.css';
+import SearchIcon from './search.svg'
+import MovieCard from './MovieCard';
+
+// Api Key afb8b459
+
+const API_URL = 'http://www.omdbapi.com?apikey=afb8b459'
+
+const App = ()=> {
+  
+  const [movies,setMovies] = useState([]);
+  const [searchTerm,setSearchTerm] = useState('')
+  const searchMovies = async(title) =>{
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json();
+
+    setMovies(data.Search);
+  }
+
+  useEffect(() =>{
+    searchMovies('Avengers')
+  },[]);
+  
   return (
-    <Router>
-      <div className="App">
-        <Routes />
+    <div className="App">
+      <h1>FilmyApp</h1>
+
+      <div className='search'>
+        <input 
+          placeholder='Search for your favorite movies'
+          value={searchTerm}
+          onChangeCapture={(e)=> setSearchTerm(e.target.value)}
+        />
+        <img 
+          src={SearchIcon}
+          alt="Search"
+          onClick={() => searchMovies(searchTerm)}
+        />
       </div>
-    </Router>
+      {
+        movies.length  > 0 ? 
+        (
+          <div className='container'> 
+            {movies.map((movie) => ( 
+              <MovieCard movie={movie} />
+            ))}
+          </div>
+        ):(
+            <div className='empty'>
+             <h2>No Movies Found</h2>
+            </div>
+          ) 
+      }
+    </div>
   );
 }
 
